5920286453cba02d078dd87ed455ab35894273a5
Mathistuber <93736246+Mathistuber@users.noreply.github.com>
2022-04-05 16:31:27 -0300

    Update Constants.php

--- model/Constants.php
+++ model/Constants.php
@@ -3,4 +3,4 @@
 define('DB_HOST','diaslagares.mysql.database.azure.com');
 define('DB_USER','matheusdias@diaslagares');
 define('DB_PASS','Ma@31032004');
-define('DB_NAME','jogadores_tb');
+define('DB_NAME','jogadorbaskabd');
